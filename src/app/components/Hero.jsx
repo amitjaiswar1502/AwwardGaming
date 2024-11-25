1f62e8c5510941d90f396440097e8d4b811767ac
AJaiswar <ajaiswar@kenmarkitan.com>
2024-11-25 10:18:38 +0530

    first push

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div
+      id="video-frame"
+      className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+    >
+      hero
+    </div>
+  );
+};
+
+export default Hero;
